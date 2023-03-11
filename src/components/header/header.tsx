3466dd9d0a24c0b77359110b43f8cbcab61fa9e5
Greg <39492220+gregorro@users.noreply.github.com>
2023-03-11 13:16:11 +0100

    feat: create mock nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,8 +5,24 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import {useState} from 'react'
+
+const pages = ['Products', 'Pricing', 'Blog'];
 
 const Header: React.FC = () => {
+
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+    };
+  
     return <>
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -17,9 +33,34 @@ const Header: React.FC = () => {
                         color="inherit"
                         aria-label="menu"
                         sx={{ mr: 2 }}
+                        onClick={handleOpenNavMenu}
                     >
                         <MenuIcon />
                     </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                <Typography textAlign="center">{page}</Typography>
+                            </MenuItem>
+                        ))}
+                    </Menu>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         News
                     </Typography>
