d9c2d98db7c794079f040863a32e69f6656fe80d
Greg <39492220+gregorro@users.noreply.github.com>
2023-03-11 11:18:06 +0100

    feat: introdice to react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'
+
+const HelloComponent: React.FC = () => {
+  return <>Hello from HelloComponent</>
+}
+
+const UserComponent: React.FC = () => {
+  return <>Hello from UserComponent</>
+}
+
+const CurrentUserComponent: React.FC = () => {
+  const params = useParams()
+  console.log('Params from CurrentUserComponent', params)
+  return <>Hello from CurrentUserComponent</>
+}
+
+const NotFoundComponent: React.FC = () => {
+  return <>NotFoundComponent</>
+}
 
 function App() {
   return (
     <div>
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HelloComponent />}/>
+          <Route path={'user'} element={<UserComponent />}/>
+          <Route path={'user/:id'} element={<CurrentUserComponent />}/>
+          <Route path={'*'} element={<NotFoundComponent />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
